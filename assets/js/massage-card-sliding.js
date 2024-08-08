6ab2e5113f38cc9ed9658c25ec8d4551302934be
arafatcnits <arafat.cnits@gmail.com>
2024-08-08 19:25:39 +0600

    facilities section ongoing

--- assets/js/massage-card-sliding.js
+++ assets/js/massage-card-sliding.js
@@ -1,3 +1,5 @@
+
+
 $(function() {
 
 	$(".prev").on('click', function(event) {
@@ -10,38 +12,70 @@ $(function() {
 		nextSlide();
 	});
 
-	if ($(".item").length <= 1) {
+	if ($(".item_card").length <= 1) {
 		$(".next").addClass('hide-nav');
 	}
 
 	$(".prev").addClass('hide-nav');
 
-	function nextSlide() {
-		var atual = $(".cd_slider").find('.current_card'),
-			next = atual.next();
+	// Auto-slide functionality
+	let autoSlideInterval = setInterval(nextSlide, 5000); // Adjust the interval (5000ms = 5s) as needed
 
-		next.addClass('current_card').removeClass('prev_slide').siblings().removeClass('current_card');
+	$(".cd_slider").hover(
+		function() {
+			clearInterval(autoSlideInterval); // Stop auto-slide on hover
+		}, 
+		function() {
+			autoSlideInterval = setInterval(nextSlide, 5000); // Restart auto-slide when not hovering
+		}
+	);
+
+	function nextSlide() {
+		var current = $(".cd_slider").find('.current_items'),
+			next = current.next();
+
+		if (!next.length) {
+			next = $(".item_card").first(); // Loop back to the first slide
+		}
+
+		current.removeClass('current_items');
+		next.addClass('current_items').removeClass('prev_slide').siblings().removeClass('current_items');
 		next.prevAll().addClass('prev_slide');
 
 		if (next.index() > 0) {
 			$(".prev").removeClass('hide-nav');
+		} else {
+			$(".prev").addClass('hide-nav');
 		}
-		if (next.index() == $(".item").last().index()) {
+
+		if (next.index() == $(".item_card").last().index()) {
 			$(".next").addClass('hide-nav');
+		} else {
+			$(".next").removeClass('hide-nav');
 		}
 	}
 
 	function prevSlide() {
-		var atual = $(".cd_slider").find('.current_card'),
-			prev = atual.prev();
+		var current = $(".cd_slider").find('.current_items'),
+			prev = current.prev();
 
-		prev.addClass('current_card').removeClass('prev_slide').siblings().removeClass('current_card');
+		if (!prev.length) {
+			prev = $(".item_card").last(); // Loop back to the last slide
+		}
 
-		if (prev.index() !== $(".item").last().index()) {
+		current.removeClass('current_items');
+		prev.addClass('current_items').removeClass('prev_slide').siblings().removeClass('current_items');
+
+		if (prev.index() !== $(".item_card").last().index()) {
 			$(".next").removeClass('hide-nav');
+		} else {
+			$(".next").addClass('hide-nav');
 		} 
+
 		if (prev.index() == 0) {
 			$(".prev").addClass('hide-nav');
+		} else {
+			$(".prev").removeClass('hide-nav');
 		}
 	}
 
